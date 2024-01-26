468656beef788e192a10958af9d678f76c6e85d7
Aditya <192105adityashah@gmail.com>
2024-01-26 20:10:37 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Landing from "./Pages/Landing/Landing";
+import Document from "./Pages/Docs/Document";
+import Navbar from "./Components/Navbar/Navbar";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Router>
+      <>
+        <Navbar />
+
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/docs" element={<Document />} />
+         
+
+          {/* PRIVATE ROUTES BELOW THIS */}
+          {/* <Route path="/" element={<Private />}>
+            <Route path="/editcustomer" element={<CustomerEdiit />} />
+          </Route> */}
+
+          
+        </Routes>
+     </>
+    </Router>
+  </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Pages/Docs/Document.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef } from "react";
+import ButtonDoc from "./Buttons";
+import CardDoc from "./Card";
+
+
+const Document = () => {
+  
+
+  return (
+    <div className="mb-12">
+    <ButtonDoc/>
+    <CardDoc/>
+    
+    </div>
+  );
+};
+
+export default Document;
